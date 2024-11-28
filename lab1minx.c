938d54934892f803ab4e286d3b49d0c0ee31af28
6704062611344 <s6704062611344@email.kmutnb.ac.th>
2024-11-28 14:47:56 +0700

    04
    ข้อ3

--- lab1minx.c
+++ lab1minx.c
@@ -3,12 +3,8 @@ int main()
 {
   int num1;
   int num2;
-  
-  printf("n1= ");
-  scanf("%d",&num1);
-  
-  printf("n2= ");
-  scanf("%d",&num2);
+   
+  scanf("%d %d",&num1,&num2);  
   
   if(num1>num2)
   {
